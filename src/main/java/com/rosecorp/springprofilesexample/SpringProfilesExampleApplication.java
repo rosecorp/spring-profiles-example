16cf54bcab8f1002f3eb55dfbea6e656de0e57a4
jakub.korczak <jakub.korczak@asos.com>
2018-02-01 14:51:12 +0000

    jk - external profiles.

--- src/main/java/com/rosecorp/springprofilesexample/SpringProfilesExampleApplication.java
+++ src/main/java/com/rosecorp/springprofilesexample/SpringProfilesExampleApplication.java
@@ -2,8 +2,13 @@ package com.rosecorp.springprofilesexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {
+		"classpath:application.yml",
+		"${external.yml}"},
+		ignoreResourceNotFound = true)
 public class SpringProfilesExampleApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/rosecorp/springprofilesexample/SpringProfilesExampleApplicationTests.java
+++ src/test/java/com/rosecorp/springprofilesexample/SpringProfilesExampleApplicationTests.java
@@ -5,15 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@PropertySource(value = {
-		"classpath:application.yml",
-		"${external.yml}"},
-		ignoreResourceNotFound = true)
 public class SpringProfilesExampleApplicationTests {
 
 	@Autowired
