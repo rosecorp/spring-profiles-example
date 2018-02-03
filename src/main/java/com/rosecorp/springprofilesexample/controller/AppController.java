730ec72a8c6d9a364112928bf55e4bde962ed8ba
jk <dakoma@gmail.com>
2018-02-03 15:56:00 +0000

    jk - custom MDC

--- src/main/java/com/rosecorp/springprofilesexample/controller/AppController.java
+++ src/main/java/com/rosecorp/springprofilesexample/controller/AppController.java
@@ -10,19 +10,38 @@ import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
+import org.slf4j.MDC;
+
 @RestController
 public class AppController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AppController.class);
+    private static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
     @Autowired
     private AppService appService;
 
     @RequestMapping(path = "/api", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public String api() {
-        LOG.info("LOG: {}", appService.read());
-        return appService.read();
+    public String api() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+
+        waitFrom500To2000();
+
+        Long estimatedTime = System.currentTimeMillis() - startTime;
+        MDC.put("execution", estimatedTime + "");
+
+        logger.info("Execution time of the query {} ", estimatedTime);
+
+        return appService.read() + " in estimated milis: " + estimatedTime;
+    }
+
+    private void waitFrom500To2000() throws InterruptedException {
+        int max = 2000;
+        int min = 500;
+        int time = new Random().nextInt((max - min) + 1) + min;
+        Thread.sleep(time);
     }
 
 }
